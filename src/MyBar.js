dc292354972013301093f70ab8a40170fcd454ef
jatuponnop <jatuponnop@gmail.com>
2022-08-11 07:48:47 +0700

    add MyBar

---
+++ src/MyBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./MyBar.css"
+
+function MyBar() {
+  return (
+    <div>
+      <ul>
+        <li><a class="active" href="/">My App</a></li>
+        <li><a href="/">News</a></li>
+        <li><a href="/">Contact</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyBar
